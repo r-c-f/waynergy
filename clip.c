183beb689f8beefda4dff497cd8a17bd4edc3899
Ryan Farley <ryan.farley@gmx.com>
2020-03-20 18:39:11 -0500

    initial stuff

--- clip.c
+++ clip.c
@@ -1,8 +1,5 @@
 #include "clip.h"
-
-int clipMonitorFd[2];
-char *clipMonitorPath[2];
-pid_t clipMonitorPid[2];
+#include "wayland.h"
 
 extern uSynergyContext synContext;
 extern char **environ;
@@ -39,6 +36,14 @@ bool clipSpawnMonitors(void)
 {
 	clipMonitorPath[0] = osGetRuntimePath("swaynergy-cb-fifo");
 	clipMonitorPath[1] = osGetRuntimePath("swaynergy-p-fifo");
+	struct sockaddr_un sa[2] = {
+		{
+			.sun_family = AF_UNIX,
+		},
+		{
+			.sun_family = AF_UNIX,
+		}
+	};
 	char *argv_0[] = {
 			"wl-paste",
 			"-n",
@@ -50,7 +55,7 @@ bool clipSpawnMonitors(void)
 	char *argv_1[] = {
 			"wl-paste",
 			"-n",
-			"--primary", 
+			"--primary",
 			"-w",
 			"swaynergy-clip-update",
 			clipMonitorPath[1],
@@ -58,14 +63,18 @@ bool clipSpawnMonitors(void)
 	};
 	char **argv[] = { argv_0, argv_1 };
 	for (int i = 0; i < 2; ++i) {
-		/*first set up FIFO*/
 		unlink(clipMonitorPath[i]);
 		errno = 0;
-		if (mkfifo(clipMonitorPath[i], 0600))
+		if ((clipMonitorFd[i] = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+			logErr("Socket call failed: %s", strerror(errno));
 			return false;
-		errno = 0;
-		if ((clipMonitorFd[i] = open(clipMonitorPath[i], O_RDWR | O_CLOEXEC)) == -1)
+		}
+		strncpy(sa.sun_path, clipMonitorPath[i], sizeof(sa.sun_path));
+		if (bind(clipMonitorFd[i], (struct sockaddr *)(sa + i), sizeof(sa[i])) == 1) {
+			logErr("Bind error: %s", strerror(errno));
 			return false;
+		}
+		while (
 		/* now we spawn */
 		errno = 0;
 		if (posix_spawnp(clipMonitorPid + i,"wl-paste",NULL,NULL,argv[i],environ)) {
@@ -96,8 +105,8 @@ void clipMonitorPollProc(struct pollfd *pfd)
 		return;
 	if (pfd->revents & POLLIN) {
 		if (!read_full(pfd->fd, &len, sizeof(len), 0)) {
-			logErr("Clipboard monitor could not read size: %s", strerror(errno));
-			goto error;
+			logErr("Clip monitor poll proc failed");
+			return;
 		}
 		char buf[len];
 		if (!read_full(pfd->fd, buf, len, 0)) {
@@ -118,7 +127,7 @@ bool clipWlCopy(enum uSynergyClipboardId id, const unsigned char *data, size_t l
 {
 	pid_t pid;
 	posix_spawn_file_actions_t fa;
-	char *argv_0[] = { 
+	char *argv_0[] = {
 			"wl-copy",
 			"-f",
 			NULL};
@@ -140,7 +149,7 @@ bool clipWlCopy(enum uSynergyClipboardId id, const unsigned char *data, size_t l
 	posix_spawn_file_actions_init(&fa);
 	posix_spawn_file_actions_adddup2(&fa, fd[0], STDIN_FILENO);
 	posix_spawn_file_actions_addclose(&fa, fd[1]);
-   	/* now we can spawn */	
+   	/* now we can spawn */
 	errno = 0;
         if (posix_spawnp(&pid, "wl-copy", &fa, NULL, argv[id], environ)) {
 		logErr("wl-copy spawn failed: %s", strerror(errno));
