6053e16b5fe841db964bee591d747108192a6a60
Ryan Farley <ryan.farley@gmx.com>
2021-01-27 01:50:49 -0600

    more

--- synergy.c
+++ synergy.c
@@ -78,3 +78,58 @@ void synSendClipboard(struct synContext *ctx, unsigned char id, uint32_t len, co
 	send_reply(ctx);
 }
 
+static char *implementations[] = {
+	"Barrier",
+	"Synergy",
+	NULL
+};
+static char *is_welcome(struct sspBuf *msg)
+{
+	char **i;
+	for (i = implementations; *i; ++i) {
+		if (strlen(*i) > msg->len)
+			continue;
+		if (memcmp(msg->data, *i, strlen(*i)) == 0) {
+			sspSeek(msg, strlen(*i));
+			return *i;
+		}
+	}
+	return NULL;
+}
+#define PARSE_ERROR() do { logErr("Parsing Error: %s %s:%d", __func__, __FILE__, __LINE__); return; } while (0)
+static void process_message(struct synContext *ctx, struct sspBuf *msg)
+{
+        const char *imp;
+        char pkt_id[5] = {0};
+	struct sspBuf *reply = ctx->reply_buf;
+        if ((imp = is_welcome(msg)))
+        {
+                // Welcome message
+                //              kMsgHello                       = "Synergy%2i%2i"
+                //              kMsgHelloBack           = "Synergy%2i%2i%s"
+                uint16_t server_major, server_minor;
+                if (!(sspNetU16(msg, &server_major) && sspNetU16(msg, &server_minor))) {
+                        PARSE_ERROR();
+                }
+                logInfo("Server is %s %" PRIu16 ".%" PRIu16, imp, server_major, server_minor);
+                sspAddString(reply, imp);
+                sspAddNetU16(reply, USYNERGY_PROTOCOL_MAJOR);
+                sspAddNetU16(reply, USYNERGY_PROTOCOL_MINOR);
+                sspAddNetU32(reply, (uint32_t)strlen(reply->m_clientName));
+                sspAddString(reply, reply->m_clientName);
+                if (!send_reply(ctx, reply))
+                {
+                        // Send reply failed, let's try to reconnect
+                        logErr("SendReply failed, trying to reconnect in a second");
+                        context->m_connected = false;
+                        context->m_sleepFunc(context->m_cookie, 1000);
+                }
+                else
+                {
+                        // Let's assume we're connected
+                        logInfo("Connected as client \"%s\"", context->m_clientName);
+                        context->m_hasReceivedHello = true;
+                        context->m_implementation = imp;
+                }
+                return;
+        }
