896353199e5162d0a63da7d4480f225feffb929a
Ryan Farley <ryan.farley@gmx.com>
2021-02-15 23:16:43 -0600

    update fdio_full, sopt

--- fdio_full.h
+++ fdio_full.h
@@ -1,4 +1,21 @@
-
+/* fdio_full -- read/write without shortness
+ *
+ * Version 1.0
+ *
+ * Copyright 2021 Ryan Farley <ryan.farley@gmx.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
 #ifndef FDIO_FULL_H_INC
 #define FDIO_FULL_H_INC
 
@@ -15,7 +32,6 @@ enum fdio_full_flag {
 	FDIO_FULL_FLAG_INTR = 2, //bail out on EINTR
 };
 
-/* functions to do read/write without shortness */
 #ifdef __GNUC__
 __attribute__((unused))
 #endif
